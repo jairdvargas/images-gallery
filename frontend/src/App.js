b8062b30ffaf645c41175e48aed882031bfc86f6
Jair <jairdvargas@icloud.com>
2022-01-30 17:26:55 -0400

    Correcciones con Lint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,42 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
-import {useState} from 'react';
+import { useState } from 'react';
 
-const UNSPLASH_KEY=process.env.REACT_APP_UNPSASH_KEY
+const UNSPLASH_KEY = process.env.REACT_APP_UNPSASH_KEY;
 
 const App = () => {
-  const [palabra, definirPalabra] = useState('')
-  
+  const [palabra, definirPalabra] = useState('');
+
   const manejadorSearchSubmit = (e) => {
     e.preventDefault();
     console.log(palabra);
     //console.log(e.target[0].value);
     //console.log(e.target[0].value);
-    fetch(`https://api.unsplash.com/photos/random/?query=${palabra}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${palabra}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        
       })
       .catch((err) => {
         console.log(err);
-      })
-      // Sirve para limpiar la casilla de busqueda una vez que se termina de buscar
-      definirPalabra('');
+      });
+    // Sirve para limpiar la casilla de busqueda una vez que se termina de buscar
+    definirPalabra('');
   };
 
   return (
     <div className="App">
-      <Header titulo="Galeria de Imagenes_"/>
-      <Search palabra={palabra} definirPalabra={definirPalabra} manejadorSubmit={manejadorSearchSubmit} />
+      <Header titulo="Galeria de Imagenes_" />
+      <Search
+        palabra={palabra}
+        definirPalabra={definirPalabra}
+        manejadorSubmit={manejadorSearchSubmit}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 
-const navbarStyle= {
-    backgroundColor:'lightblue'
+const navbarStyle = {
+  backgroundColor: 'lightblue',
 };
 
-const Header = ({titulo}) => {
-
-    return (
-        <Navbar style={navbarStyle}  variant="light">
-            <Container>
-                <Navbar.Brand href="/">{titulo}</Navbar.Brand>
-            </Container>
-      </Navbar>
-    );
+const Header = ({ titulo }) => {
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">{titulo}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
