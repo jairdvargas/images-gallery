12d2acf6f4879d63e7d9094b2983117a4490d26b
JairUbuntu <jairdvargas@icloud.com>
2022-02-01 10:23:21 -0400

    Agregar API python y ambiente virtual

---
+++ API/main.py
@@ -0,0 +1,6 @@
+#antes de todo se tiene que crear la carpeta .venv y usar el comando
+#pipenv install flask
+#se crea primero la carpeta .venv para que los archivos se copien alla del ambiente virtual.
+#luego se tiene que activar el shell virtual con:  pipenv shell
+#Cuando se va a recrear el ambiente virtual solo se usa: pipenv install
+# #por eso se ignora la carpeta .venv en github porque se puede recrear localmente. 
