194a4ef01bb1f00deb42c82242d0087ef36ba633
Jair <jairdvargas@icloud.com>
2022-02-01 23:55:16 -0400

    Se agrego API en python

--- API/main.py
+++ API/main.py
@@ -7,13 +7,38 @@
 # Para poder ejecutar flask en linux se tiene q poder en terminal el comnado: export FLASK_APP=main
 #luego se tiene que ejecutar flask con: flask run
 
-from flask import Flask
+#from wsgiref import headers
+import requests
+import os
+from flask import Flask, request
+from dotenv import load_dotenv
 
+#cargar el archivo de variable donde esta la llave del api
+load_dotenv(dotenv_path="./.env.local")
+
+UNSPLASH_URL="https://api.unsplash.com/photos/random"
+UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
+
+#revisar si existe la llave api
+if not UNSPLASH_KEY:
+  raise EnvironmentError("Por favor crear el archivo .env.local y agregar UNSPLASH_KEY")
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+
+@app.route("/nueva-imagen")
+def nueva_imagen():
+    palabra=request.args.get("query")
+    headers={
+      "Accept-Version" : "v1",
+      "Authorization" : "Client-ID " + UNSPLASH_KEY
+    }
+    params={
+      "query" : palabra
+    }
+    respuesta=requests.get(url=UNSPLASH_URL,headers=headers, params=params)
+    data=respuesta.json()
+    return data
 
 if __name__=="__main__":
   app.run("0.0.0.0", port=5050)
